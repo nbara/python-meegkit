581346dfc27082937ef3c160e37a5fa462503229
Pedro Alcocer <pealco@gmail.com>
2008-12-13 14:49:01 -0500

    SNS out is not longer zero. I'm not sure if it is accurate though.

--- sns.py
+++ sns.py
@@ -31,6 +31,58 @@ def sns(data, nneighbors = 0, skip = 0, w = array([])):
     return y
 
 
+def sns0(c, nneighbors, skip=0, wc=[]):
+    """docstring for sns0"""
+    
+    if not any(wc):
+        wc = c
+        
+    n = c.shape[0]
+    
+    if not nneighbors:
+        nneighbors = n - 1
+        
+    r = zeros(c.shape)
+    
+    # normalize
+    d = sqrt(1 / diag(c))
+    c = c * d * d.T
+    
+    for k in xrange(n):
+        c1 = c[:, k] # correlation of channel k with all other channels
+        idx = argsort(c1**2, 0)[::-1] # sort by correlation, descending order
+        c1 = c1[idx]
+        idx = idx[skip:skip+nneighbors] # keep best
+        print "c1", c1.shape
+        print "idx", idx
+        
+        # pca neighbors to orthogonalize them
+        c2 = wc[idx, :][:, idx]
+        [topcs, eigenvalues] = pcarot(c2)
+        topcs = dot(topcs, diag(1/sqrt(eigenvalues)))
+        print "c2", c2.shape
+        
+        # augment rotation matrix to include this channel
+        stack1 = hstack((1, zeros(topcs.shape[0])))
+        stack2 = hstack((zeros((topcs.shape[0], 1)), topcs))
+        topcs = vstack((stack1, stack2))
+        
+        # correlation matrix for rotated data
+        #c3 = topcs.T * wc[hstack((k,idx)), hstack((k,idx))] * topcs
+        c3 = dot(dot(topcs.T, wc[hstack((k, idx)), :][:, hstack((k, idx))]), topcs)
+        print "c3", c3.shape
+        
+        # first row defines projection to clean component k
+        c4 = dot(c3[0, 1:], topcs[1:, 1:].T)
+        c4.shape = (c4.shape[0], 1)
+        print "c4", c4
+        
+        # insert new column into denoising matrix
+        r[idx, k] = squeeze(c4)
+    
+    return r
+
+
 def sns1(x, nneighbors, skip):
     """docstring for sns1"""
     if x.ndim > 2:
@@ -76,53 +128,4 @@ def sns1(x, nneighbors, skip):
     
     return y
 
-
-def sns0(c, nneighbors, skip=0, wc=[]):
-    """docstring for sns0"""
-    
-    if not any(wc):
-        wc = c
-        
-    n = c.shape[0]
-    
-    if not nneighbors:
-        nneighbors = n - 1
-        
-    r = zeros(c.shape)
-    
-    # normalize
-    d = sqrt(1 / diag(c))
-    c = c * d * d.T
-    
-    for k in xrange(n):
-        
-        c1 = c[:, k] # correlation of channel k with all other channels
-        corr_sq = c1 ** 2
-        idx = argsort(corr_sq, 0)[::-1] # sort by correlation, descending order
-        c1 = c1[idx]
-        idx = idx[skip+2:skip+1+nneighbors+1] # keep best
-        
-        # pca neighbors to orthogonalize them
-        c2 = wc[idx, :][:, idx]
-        [topcs, eigenvalues] = pcarot(c2)
-        topcs = topcs * diag(1/sqrt(eigenvalues))
-        
-        # augment rotation matric to include this channel
-        stack1 = hstack((1, zeros(topcs.shape[0])))
-        stack2 = hstack((zeros((topcs.shape[0], 1)), topcs))
-        topcs = vstack((stack1, stack2))
-        
-        # correlation matrix for rotated data
-        #c3 = topcs.T * wc[c_[k,idx],c_[k,idx]] * topcs
-        idx.shape = (idx.shape[0], 1)
-        c3 = topcs.T * wc[vstack((k, idx)), vstack((k, idx))] * topcs
-        
-        # first row defines projection to clean component k
-        c4 = dot(c3[0, 1:], topcs[1:, 1:].T)
-        c4.shape = (c4.shape[0], 1)
-        
-        # insert new column into denoising matrix
-        r[idx, k] = c4
-        
-    return r
 
