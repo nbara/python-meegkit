12bc2bd1f30580e2be27f5de39f49796263a3c63
nbara <10333715+nbara@users.noreply.github.com>
2020-11-12 12:09:59 +0100

    Output maps + update doc + add example

--- meegkit/utils/__init__.py
+++ meegkit/utils/__init__.py
@@ -6,8 +6,9 @@ from .covariances import (block_covariance, convmtx, cov_lags,
                           nonlinear_eigenspace, pca, regcov, tscov, tsxcov)
 from .denoise import (demean, find_outlier_samples, find_outlier_trials,
                       mean_over_trials, wpwr)
-from .matrix import (fold, multishift, multismooth, normcol, relshift, shift,
-                     shiftnd, theshapeof, unfold, unsqueeze, widen_mask)
+from .matrix import (fold, matmul3d, multishift, multismooth, normcol,
+                     relshift, shift, shiftnd, theshapeof, unfold, unsqueeze,
+                     widen_mask)
 from .sig import (gaussfilt, hilbert_envelope, slope_sum, smooth,
                   spectral_envelope, teager_kaiser)
 from .stats import (bootstrap_ci, bootstrap_snr, cronbach, rms, robust_mean,
