589d1087ed6bb44a06a58db74c348354158fde6d
Juan Jesus Torre <juanjesustorre@gmail.com>
2021-02-22 14:39:29 +0100

    RESS: style revision

--- meegkit/ress.py
+++ meegkit/ress.py
@@ -32,7 +32,7 @@ def RESS(X, sfreq: int, peak_freq: float, neig_freq: float = 1,
         If True, also output maps (mixing matrix).
     return_comps : bool
         If True, also output eigenvectors used to transform the data into RESS 
-        components
+        components.
 
     Returns
     -------
@@ -41,7 +41,7 @@ def RESS(X, sfreq: int, peak_freq: float, neig_freq: float = 1,
     maps : array, shape=(n_channels, n_keep)
         If return_maps is True, also output mixing matrix.
     comps : array, shape=(n_channels, n_keep)
-        If return_comps is True, also output eigenvectors
+        If return_comps is True, also output eigenvectors.
 
     Notes
     -----
@@ -112,6 +112,8 @@ def RESS(X, sfreq: int, peak_freq: float, neig_freq: float = 1,
     out = np.zeros((n_samples, n_keep, n_trials))
     for t in range(n_trials):
         out[..., t] = X[:, :, t] @ comps
+
+    from IPython import embed;embed()
 
     if return_maps and return_comps:
         return out, maps, V
