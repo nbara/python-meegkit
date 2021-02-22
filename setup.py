0676e2fd1dd9da588058855970306d304df93a08
nicolas barascud <10333715+nbara@users.noreply.github.com>
2021-02-22 16:50:08 +0100

    Bump version + fix sklearn/pyriemann compat (#29)

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='N Barascud',
     author_email='nicolas.barascud@ens.fr',
     license='UNLICENSED',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(exclude=['doc', 'tests']),
     zip_safe=False)
