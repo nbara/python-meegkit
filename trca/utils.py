ce07a1564958911d9c5efb677624016558e64f5d
Giuseppe <giuseppe.ferraro@isae-supaero.fr>
2021-03-23 16:23:24 +0100

    cleaning and editing utils

--- trca/utils.py
+++ trca/utils.py
@@ -3,10 +3,38 @@ import numpy as np
 import scipy 
 
 def round_half_up(n, decimals=0):
+    """
+    This function rounds the last decimal of the number as below:
+    from 0 to 4 rounds down
+    from 5 to 9 rounds up
+
+    Parameters
+    ----------
+    n: number to round
+    decimals : number of decimals
+
+    Returns
+    ---------
+    n rounded
+    """ 
     multiplier = 10 ** decimals
     return int(math.floor(n*multiplier + 0.5) / multiplier)
 
 def normfit(data, confidence=0.95):
+    """
+    Parameters
+    ----------
+    data: array of numbers
+    confidence : interval of confidence. Default = 0.95.
+
+    Returns
+    -------
+    m : mean
+    sigma : std deviation
+    [m - h, m + h] : confidence interval of the mean
+    [sigmaCI_lower, sigmaCI_upper] : confidence interval of the std
+    """
+
     a = 1.0 * np.array(data)
     n = len(a)
     m, se = np.mean(a), scipy.stats.sem(a)
