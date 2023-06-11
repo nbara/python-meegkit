503756bf60b9ebf4151bc8ec8de73a939f1ca562
John Kyle Cooper <jkcooper@aggienetwork.com>
2023-06-11 15:52:40 +0200

    fixed formatting according to flake8

--- examples/example_mcca_2.py
+++ examples/example_mcca_2.py
@@ -3,15 +3,26 @@ Example 1 - sinusoidal target in separable noise.
 =================================================
 reproduced from de Cheveigné et al. (2018).
 
-Synthetic data for this example consisted of 10 data matrices, each of dimensions 10000 samples 
-10 channels. Each was obtained by multiplying 9 Gaussian noise time series (independent and uncorrelated) by a 9 x 10 mixing matrix with random Gaussian coefficients. To this background of noise was added a “target” consisting of a sinusoidal time series multiplied by a 1 x 10 mixing matrix with random coefficients. The target was the same for all data matrices, but the mixing matrices differed, as did the noise matrices. The SNR was set to 10−20, i.e. a very unfavorable SNR. The noise is of rank 9 and the signal of rank 1, so signal and noise are in principle linearly separable.
+Synthetic data for this example consisted of 10 data matrices,
+each of dimensions 10000 samples x 10 channels. Each was obtained
+by multiplying 9 Gaussian noise time series (independent and uncorrelated)
+by a 9 x 10 mixing matrix with random Gaussian coefficients.
+To this background of noise was added a “target” consisting of a sinusoidal
+time series multiplied by a 1 x 10 mixing matrix with random coefficients.
+The target was the same for all data matrices,
+but the mixing matrices differed,
+as did the noise matrices. The SNR was set to 10−20,
+i.e. a very unfavorable SNR.
+The noise is of rank 9 and the signal of rank 1,
+so signal and noise are
+in principle linearly separable.
 
 """
 
 # %%
+from meegkit import cca
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
 
 # %%
 # Set the seed for the random number generator for reproducibility
@@ -26,8 +37,10 @@ signal_rank = 1
 unfavorable_SNR_dB = -20  # SNR in decibels
 
 # Generate noise matrices and mixing matrices
-noise_matrices = [np.random.normal(size=(num_samples, noise_rank)) for _ in range(num_matrices)]
-mixing_matrices = [np.random.normal(size=(noise_rank, num_channels)) for _ in range(num_matrices)]
+noise_matrices = [np.random.normal(size=(num_samples, noise_rank))
+                  for _ in range(num_matrices)]
+mixing_matrices = [np.random.normal(size=(noise_rank, num_channels))
+                   for _ in range(num_matrices)]
 
 # Generate sinusoidal target
 t = np.linspace(0, 1, num_samples)
@@ -60,8 +73,6 @@ for i in range(num_matrices):
 x = np.concatenate(data_matrices, axis=-1)
 
 # %%
-from meegkit import cca
-
 # Compute Covariance matrix
 C = np.dot(x.T, x)
 
@@ -70,7 +81,7 @@ A, score, AA = cca.mcca(C, 10)
 
 # %%
 # Compute the recovered signal using first SC
-x_recovered = x.dot(A)[:,0]
+x_recovered = x.dot(A)[:, 0]
 # Normalize the recovered signal
 x_recovered = x_recovered / x_recovered.std()
 # Compute variance across SCs
@@ -80,20 +91,18 @@ variance = np.var(x.dot(A), axis=0)
 # Plot the results
 fig, ax = plt.subplots(1, 4, figsize=(12, 4))
 ax[0].plot(target_signal)
-ax[0].set_title('Target')
-ax[0].set_xlabel('Sample')
-ax[0].set_ylabel('Amplitude')
+ax[0].set_title("Target")
+ax[0].set_xlabel("Sample")
+ax[0].set_ylabel("Amplitude")
 ax[1].plot(data_matrix)
-ax[1].set_title('Target + Noise')
-ax[1].set_ylabel('Amplitude')
-ax[1].set_xlabel('Sample')
-ax[2].plot(variance, 'o-k')
-ax[2].set_xlabel('SC')
-ax[2].set_ylabel('Variance')
+ax[1].set_title("Target + Noise")
+ax[1].set_ylabel("Amplitude")
+ax[1].set_xlabel("Sample")
+ax[2].plot(variance, "o-k")
+ax[2].set_xlabel("SC")
+ax[2].set_ylabel("Variance")
 ax[3].plot(x_recovered)
-ax[3].set_title('Recovered')
-ax[3].set_ylabel('Amplitude')
-ax[3].set_xlabel('Sample')
+ax[3].set_title("Recovered")
+ax[3].set_ylabel("Amplitude")
+ax[3].set_xlabel("Sample")
 plt.tight_layout()
-
-
