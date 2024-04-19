a9b56fb39c7253de434e02ef1b1baed1b9a7a785
Nicolas Barascud <10333715+nbara@users.noreply.github.com>
2024-04-19 12:59:22 +0200

    update pyriemann

--- meegkit/utils/covariances.py
+++ meegkit/utils/covariances.py
@@ -33,10 +33,10 @@ def block_covariance(data, window=128, overlap=0.5, padding=True, estimator="cov
         Block covariance.
 
     """
-    from pyriemann.utils.covariance import _check_est
+    from pyriemann.utils.covariance import check_function, cov_est_functions
 
     assert 0 <= overlap < 1, "overlap must be < 1"
-    est = _check_est(estimator)
+    est = check_function(estimator, cov_est_functions)
     cov = []
     n_chans, n_samples = data.shape
     if padding:  # pad data with zeros
