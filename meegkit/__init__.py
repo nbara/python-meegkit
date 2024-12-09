9d8f785c1a289664760bf61e0277ab38e9df6b2e
Nicolas Barascud <10333715+nbara@users.noreply.github.com>
2024-12-09 09:43:19 +0100

    Execute notebooks

--- meegkit/__init__.py
+++ meegkit/__init__.py
@@ -1,7 +1,7 @@
 """M/EEG denoising utilities in python."""
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
-from . import asr, cca, detrend, dss, lof, ress, sns, star, trca, tspca, utils
+from . import asr, cca, detrend, dss, lof, phase, ress, sns, star, trca, tspca, utils
 
-__all__ = ["asr", "cca", "detrend", "dss", "lof", "ress", "sns", "star", "trca",
-           "tspca", "utils"]
+__all__ = ["asr", "cca", "detrend", "dss", "lof", "phase", "ress", "sns", "star",
+           "trca", "tspca", "utils"]
